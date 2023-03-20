ef3b226db5084589a7b1f84b6669642b90f7010e
Ariana Ledezma <127069106+ariaLed72@users.noreply.github.com>
2023-03-19 19:50:00 -0700

    Updated JS and CSS, addition of weather Icon

--- src/index.js
+++ src/index.js
@@ -20,7 +20,13 @@ document.addEventListener("DOMContentLoaded", function changeDate() {
   let date = now.getDate();
   let year = now.getFullYear();
   let hour = now.getHours();
+  if (hour < 10) {
+    hours = `0${hours}`;
+  }
   let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
 
   let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
   let day = days[now.getDay()];
@@ -28,54 +34,84 @@ document.addEventListener("DOMContentLoaded", function changeDate() {
   h3.innerHTML = `${hour}:${minutes} ${day} ${month} ${date}, ${year}`;
 });
 
-let apiKey = `cabdbda40038ba7d1165b953b1c7bd6c`;
-
 function search(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-engine");
   let changeCity = document.querySelector("h1");
   changeCity.textContent = `${searchInput.value}`;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=${apiKey}&units=imperial`;
-  searchInput.value = "";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${searchInput.value}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(changeTemp);
 }
 
-let form = document.querySelector("form");
-form.addEventListener("submit", search);
-
 function changeTemp(response) {
-  console.log(response.data.main.temp);
   let wholeTemp = document.querySelector("#big-temp");
-  let temperature = Math.round(response.data.main.temp);
+  let temperature = Math.round(response.data.temperature.current);
   wholeTemp.innerHTML = `${temperature}`;
   console.log(response);
 
   let weatherPattern = document.querySelector("#description");
-  let description = response.data.weather[0].description;
+  let description = response.data.condition.description;
   weatherPattern.innerHTML = `${description}`;
 
   let humidity = document.querySelector("#humidity");
-  humidity.innerHTML = `${response.data.main.humidity}`;
+  humidity.innerHTML = response.data.temperature.humidity;
 
   let feelsLike = document.querySelector("#feels-like");
-  let feelsLikeRound = Math.round(response.data.main.feels_like);
+  let feelsLikeRound = Math.round(response.data.temperature.feels_like);
   feelsLike.innerHTML = `${feelsLikeRound}`;
 
   let wind = document.querySelector("#wind");
   let windSpeed = Math.round(response.data.wind.speed);
   wind.innerHTML = `${windSpeed}`;
+
+  let iconElement = document.querySelector("#weather-icon");
+  iconElement.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+  );
+  iconElement.setAttribute("alt", response.data.condition.description);
 }
 
-function showPosition(position) {
-  let lat = position.coords.latitude;
-  let lon = position.coords.longitude;
-  let apiUrlPosition = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
-  axios.get(apiUrlPosition).then(changeTemp);
+function showFahrenheitTemp(event) {
+  event.preventDefault();
+  let celsiusTemperature = response.data.temperature.current;
+  let fahrenheitElement = document.querySelector("#big-temp");
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  fahrenheitElement.innerHTML = Math.round(fahrenheitTemperature);
 }
 
-function getCurrentPosition(position) {
-  navigator.geolocation.getCurrentPosition(showPosition);
+function showCelsiusTemp(event) {
+  event.preventDefault();
+  let celsiusTemperature = response.data.temperature.current;
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let celsiusElement = document.querySelector("#big-temp");
+  celsiusElement.innerHTML = Math.round(celsiusTemperature);
 }
 
-let button = document.querySelector("#position-button");
-button.addEventListener("click", getCurrentPosition);
+let apiKey = "42ba1b13f6cc540e038b0aeaao0t76f8";
+
+let form = document.querySelector("form");
+form.addEventListener("submit", search);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-conversion");
+fahrenheitLink.addEventListener("click", showFahrenheitTemp);
+
+let celsiusLink = document.querySelector("#celsius-conversion");
+celsiusLink.addEventListener("click", showCelsiusTemp);
+
+//function showPosition(position) {
+// let lat = position.coordinates.lat;
+// let lon = position.coordinates.lon;
+//let apiUrlPosition = `https://api.shecodes.io/weather/v1/current?lon=${lon}&lat=${lat}&key=${apiKey}&units=metric`;
+//axios.get(apiUrlPosition).then(changeTemp);
+//}
+
+//function getCurrentPosition(position) {
+//// navigator.geolocation.getCurrentPosition(showPosition);
+//}
+
+//let button = document.querySelector("#position-button");
+//button.addEventListener("click", getCurrentPosition);
